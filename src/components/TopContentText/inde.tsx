27c0caf122ebdb0ead1394eaa129ca8db832dd9c
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-08-30 18:36:59 -0300

    responsive searchbar

--- src/components/TopContentText/inde.tsx
+++ src/components/TopContentText/inde.tsx
@@ -9,7 +9,7 @@ export function TopContentText () {
   return (  
     <motion.div variants={container} initial="hidden" animate="visible" transition={{duration: 1}}>
       <div className='mt-[4rem] max-w-[450px] mx-auto'>
-        <h2 className="text-[1.875rem] whitespace-nowrap text-center">Ofertas especiais em setembro!</h2>
+        <h2 className="text-[25px] md:text-[1.875rem] md:whitespace-nowrap text-center">Ofertas especiais em setembro!</h2>
         <p className="text-sm text-gray-500 text-center">Assine a nossa newsletter e fique por dentro das novas ofertas.</p>
       </div>
     </motion.div>
