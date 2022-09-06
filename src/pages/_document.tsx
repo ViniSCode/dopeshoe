8c20b9a8ce5f9f7ddb3d99a525714befe9606308
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-09-06 14:20:25 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Rubik+Glitch&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
         </Head>
         <body>
           <Main />
