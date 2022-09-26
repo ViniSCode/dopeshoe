005af287217db880bd7a4ccb955baccb4adb9270
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-09-26 13:35:18 -0300

    webhook problem??

--- src/pages/api/webhooks/index.ts
+++ src/pages/api/webhooks/index.ts
@@ -1,70 +1,70 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Readable } from "stream";
-import Stripe from "stripe";
-import { stripe } from "../../../services/stripe";
-import { saveCheckout } from "../_lib/manageCheckout";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { Readable } from "stream";
+// import Stripe from "stripe";
+// import { stripe } from "../../../services/stripe";
+// import { saveCheckout } from "../_lib/manageCheckout";
 
-async function buffer(readable: Readable) {
-  const chunks = [];
+// async function buffer(readable: Readable) {
+//   const chunks = [];
 
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-  }
+//   for await (const chunk of readable) {
+//     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+//   }
 
-  return Buffer.concat(chunks);
-}
+//   return Buffer.concat(chunks);
+// }
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+// const relevantEvents = new Set(["checkout.session.completed"]);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const secret = req.headers['stripe-signature'];
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === "POST") {
+//     const buf = await buffer(req);
+//     const secret = req.headers['stripe-signature'];
 
-    let event: Stripe.Event;
+//     let event: Stripe.Event;
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        buf,
-        secret!,
-        process.env.STRIPE_WEBHOOK_SECRET!
-      );
-    } catch (err: any) {
-      return res.status(400).send(`Webhook error: ${err.message}`);
-    }
+//     try {
+//       event = stripe.webhooks.constructEvent(
+//         buf,
+//         secret!,
+//         process.env.STRIPE_WEBHOOK_SECRET!
+//       );
+//     } catch (err: any) {
+//       return res.status(400).send(`Webhook error: ${err.message}`);
+//     }
 
-    const { type } = event;
+//     const { type } = event;
 
-    if (relevantEvents.has(type)) {
-      try {
-        switch (type) {
-          case "checkout.session.completed":
-            const checkoutSession = event.data
-              .object as Stripe.Checkout.Session;
+//     if (relevantEvents.has(type)) {
+//       try {
+//         switch (type) {
+//           case "checkout.session.completed":
+//             const checkoutSession = event.data
+//               .object as Stripe.Checkout.Session;
 
-            await saveCheckout(
-              checkoutSession.id.toString(),
-              checkoutSession.customer!.toString(),
-              checkoutSession.metadata
-            );
-            break;
-          default:
-            throw new Error("Unhandled event.");
-        }
-      } catch (err) {
-        return res.json({ error: "Webhook handler failed." });
-      }
-    }
+//             await saveCheckout(
+//               checkoutSession.id.toString(),
+//               checkoutSession.customer!.toString(),
+//               checkoutSession.metadata
+//             );
+//             break;
+//           default:
+//             throw new Error("Unhandled event.");
+//         }
+//       } catch (err) {
+//         return res.json({ error: "Webhook handler failed." });
+//       }
+//     }
 
-    res.json({ received: true });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
-};
+//     res.json({ received: true });
+//   } else {
+//     res.setHeader("Allow", "POST");
+//     res.status(405).end("Method Not Allowed");
+//   }
+// };
